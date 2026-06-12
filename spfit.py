26c083f93e56b7c2e7aff3af603a83e59bd0214d
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-06-12 14:34:14 -0400

    Update spfit.py

--- spfit.py
+++ spfit.py
@@ -25,7 +25,7 @@ BASE_URL = "https://streamseast.is"
 VLC_OUTPUT = "spfit_vlc.m3u8"
 TIVIMATE_OUTPUT = "spfit_tivimate.m3u8"
 
-# Headers (NOT encoded for TiviMate)
+# Headers
 USER_AGENT = "Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F120.0.0.0%20Safari%2F537.36"
 REFERER = "https://sportspass.fit/"
 ORIGIN = "https://sportspass.fit"
