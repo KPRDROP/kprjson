73bc53ceebf9c45bcf33a2c41be1adf7e91df6dd
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-07-03 17:38:03 -0400

    Update sport_ir.py

--- sport_ir.py
+++ sport_ir.py
@@ -5,7 +5,7 @@ import re
 from functools import partial
 from urllib.parse import urljoin, quote
 
-from utils import Cache, Time, get_logger, leagues, network
+from utils import Cache, Event, Time, get_logger, leagues, network
 
 log = get_logger(__name__)
 
