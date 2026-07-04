1ed0818d17faa3f7c5021db70191847ff536b763
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-07-04 14:35:28 -0400

    Update REFERER and ORIGIN URLs in spfit.py

--- spfit.py
+++ spfit.py
@@ -27,8 +27,8 @@ TIVIMATE_OUTPUT = "spfit_tivimate.m3u8"
 
 # Headers
 USER_AGENT = "Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F120.0.0.0%20Safari%2F537.36"
-REFERER = "https://sportspass.fit/"
-ORIGIN = "https://sportspass.fit"
+REFERER = "https://sportspass.shop/"
+ORIGIN = "https://sportspass.shop"
 
 # Sport categories
 SPORT_CATEGORIES = {
